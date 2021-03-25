3d8f16db67d28774adb1921e5829385db3090f20
alexandars62-36 <79636999+alexandars62-36@users.noreply.github.com>
2021-03-25 15:45:25 +0300

    Add files via upload

---
+++ js/spectral_composition.js
@@ -0,0 +1,69 @@
+function changeItem_2() {
+    document.getElementById('img_2').style.display = 'flex';
+    document.getElementById('sort_button__img_1').style.opacity = '0.5';
+    document.getElementById('t_1').style.display = 'none';
+    document.getElementById('t_2').style.display = 'inline-block';
+   
+  }
+  function rechangeItem_2() {
+    document.getElementById('img_2').style.display = 'none';
+    document.getElementById('sort_button__img_1').style.opacity = '1';
+    document.getElementById('t_2').style.display = 'none';
+    document.getElementById('t_1').style.display = 'inline-block';
+  }
+  
+
+  function changeItem_3() {
+    document.getElementById('img_3').style.display = 'flex';
+    document.getElementById('sort_button__img_1').style.opacity = '0.5';
+    document.getElementById('t_1').style.display = 'none';
+    document.getElementById('t_3').style.display = 'inline-block';
+  }
+  function rechangeItem_3() {
+    document.getElementById('img_3').style.display = 'none';
+    document.getElementById('sort_button__img_1').style.opacity = '1';
+    document.getElementById('t_3').style.display = 'none';
+    document.getElementById('t_1').style.display = 'inline-block';
+  }
+
+
+  function changeItem_4() {
+    document.getElementById('img_4').style.display = 'flex';
+    document.getElementById('sort_button__img_1').style.opacity = '0.5';
+    document.getElementById('t_1').style.display = 'none';
+    document.getElementById('t_4').style.display = 'inline-block';
+  }
+  function rechangeItem_4() {
+    document.getElementById('img_4').style.display = 'none';
+    document.getElementById('sort_button__img_1').style.opacity = '1';
+    document.getElementById('t_4').style.display = 'none';
+    document.getElementById('t_1').style.display = 'inline-block';
+  }
+
+
+  function changeItem_5() {
+    document.getElementById('img_5').style.display = 'flex';
+    document.getElementById('sort_button__img_1').style.opacity = '0.5';
+    document.getElementById('t_1').style.display = 'none';
+    document.getElementById('t_5').style.display = 'inline-block';
+  }
+  function rechangeItem_5() {
+    document.getElementById('img_5').style.display = 'none';
+    document.getElementById('sort_button__img_1').style.opacity = '1';
+    document.getElementById('t_5').style.display = 'none';
+    document.getElementById('t_1').style.display = 'inline-block';
+  }
+
+
+  function changeItem_6() {
+    document.getElementById('img_6').style.display = 'flex';
+    document.getElementById('sort_button__img_1').style.opacity = '0.5';
+    document.getElementById('t_1').style.display = 'none';
+    document.getElementById('t_6').style.display = 'inline-block';
+  }
+  function rechangeItem_6() {
+    document.getElementById('img_6').style.display = 'none';
+    document.getElementById('sort_button__img_1').style.opacity = '1';
+    document.getElementById('t_6').style.display = 'none';
+    document.getElementById('t_1').style.display = 'inline-block';
+  }
